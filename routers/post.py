5ebb8042b1161dd0726818681d8dfa159f7e556c
Stepan <takistepan@gmail.com>
2023-01-26 18:59:17 +0300

    Добавлена возможность добавления нового поста

---
+++ routers/post.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, status
+from routers.schemas import PostBase, PostDisplay
+from sqlalchemy.orm.session import Session
+from db.database import get_db
+from db import db_post
+from fastapi.exceptions import HTTPException
+
+router = APIRouter(
+    prefix='/post',
+    tags=['post']
+)
+
+image_url_types = ['absolute', 'relative']
+
+@router.post('', response_model=PostDisplay)
+def create_post(request: PostBase, db: Session = Depends(get_db)):
+    if not request.image_url_type in image_url_types:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, 
+        detail="Параметр image_url_type принимает значение либо 'absolyte', либо 'relative'.")
+    return db_post.create_post(db, request)
