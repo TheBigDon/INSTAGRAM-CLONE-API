b803afc0007586cf6810d941788f683c6cc2342d
Stepan <takistepan@gmail.com>
2023-01-26 15:28:07 +0300

    Создана таблица с пользователями

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from db import models
+from db.database import engine
+
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return "Hi!"
+
+models.Base.metadata.create_all(engine)    
