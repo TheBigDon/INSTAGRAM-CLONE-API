14252ab73ed27ffb65edaa648bc35960bf62a614
Stepan <takistepan@gmail.com>
2023-01-27 17:03:02 +0300

    Добавлена возможность удаления поста

--- db/db_post.py
+++ db/db_post.py
@@ -2,6 +2,7 @@ from db.models import DbPost
 from routers.schemas import PostBase
 from sqlalchemy.orm.session import Session
 import datetime
+from fastapi import HTTPException, status
 
 
 def create_post(db: Session, request: PostBase):
@@ -20,3 +21,16 @@ def create_post(db: Session, request: PostBase):
 
 def get_all_posts(db: Session):
     return db.query(DbPost).all()
+
+def delete_post(db: Session, id: int, user_id: int):
+    post = db.query(DbPost).filter(DbPost.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+        detail=f'Пост с id {id} не найден.')
+    if post.user_id != user_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, 
+        detail='Только автор поста может удалить пост.')
+    
+    db.delete(post)
+    db.commit()
+    return 'ok'
