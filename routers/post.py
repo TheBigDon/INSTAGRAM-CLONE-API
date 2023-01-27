fc80591f17172839c8dc52795acac37a28ab8ce5
Stepan <takistepan@gmail.com>
2023-01-27 14:08:25 +0300

    Добавлена возможность получения всех постов пользователей

--- routers/post.py
+++ routers/post.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm.session import Session
 from db.database import get_db
 from db import db_post
 from fastapi.exceptions import HTTPException
+from typing import List
 
 router = APIRouter(
     prefix='/post',
@@ -18,3 +19,7 @@ def create_post(request: PostBase, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, 
         detail="Параметр image_url_type принимает значение либо 'absolute', либо 'relative'.")
     return db_post.create_post(db, request)
+
+@router.get('/all', response_model=List[PostDisplay])
+def get_all_posts(db: Session = Depends(get_db)):
+    return db_post.get_all_posts(db)
